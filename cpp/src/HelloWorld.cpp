8587b8ff29552736c7060272852c96f7f5836714
BAICH, Essadiq <essadiq.baich-prestataire@ca-cib.com>
2018-05-11 19:21:59 +0200

    Add C++ project setting for Eclipse

---
+++ cpp/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Spaich
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int helloWorld() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
